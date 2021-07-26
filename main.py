d64f0f2fa9264ccedf16d2558ce8c8638f6f67c0
Shouguang Zhang <495140158@qq.com>
2021-07-26 22:38:25 +0800

    main

---
+++ main.py
@@ -0,0 +1,6 @@
+from util import read_file
+
+ios = read_file("./db/ios.txt").splitlines()
+safari_build = read_file("./db/safari_build.txt").splitlines()
+safari_version = read_file("./db/safari_version.txt").splitlines()
+webkit = read_file("./db/webkit.txt").splitlines()
